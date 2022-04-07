5a83bcd138cd65e85ecd5abd2a39e7a84a4d483d
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-04-06 19:09:10 -0500

    Solucion del ejercicio 1

--- main.js
+++ main.js
@@ -3,18 +3,29 @@ let cajaMensaje = document.querySelector(".solucion");
 
 let solucion = async()=>{
     let config = {
-        method : "POST",
+        method : from.method,
         body: JSON.stringify({
-            num1 : document.querySelector("#num1").value,
-            num2 : document.querySelector("#num2").value
+            num1 : Number(document.querySelector("#num1").value),
+            num2 : Number(document.querySelector("#num2").value)
         })
     };
-    let peticion = await fetch("ejercicio1.php", config);
-    let data = await peticion.text();
-    cajaMensaje.insertAdjacentText("beforeend", data);
+    let peticion = await fetch(from.action, config);
+    let res = await peticion.json();
+    cajaMensaje.insertAdjacentHTML("beforeend", `<div>Respuesta del servidor: <b>${res.server}</b> ${res.respuesta}</div`);
 }
 from.addEventListener("submit", (e)=>{
-    solucion();
+    switch (e.submitter.dataset.accion) {
+        case "enviar":
+            solucion();
+            break;
+        case "limpiar":
+            cajaMensaje.innerHTML = "";
+            from.reset();
+            break;
+        default:
+            cajaMensaje.insertAdjacentHTML("beforeend", `<div>El <b>data-accion='${e.submitter.dataset.accion}'</b> no esta configurada</div>`);
+            break;
+    }
     e.preventDefault();
 })
 
